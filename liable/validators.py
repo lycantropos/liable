665aecfcf5d429b42cc46765a200fbdabe7b8f9e
Ibrakov Azat <azatibrakov@gmail.com>
2017-12-19 19:49:20 +0700

    Added 'validate_modules' validator for checking modules accessibility

--- liable/validators.py
+++ liable/validators.py
@@ -1,3 +1,4 @@
+import importlib.util
 import os
 from itertools import filterfalse
 from typing import Iterable
@@ -18,3 +19,31 @@ def validate_paths(paths: Iterable[str]) -> None:
                '{paths}.'
                .format(paths=non_existent_paths_str))
     raise FileNotFoundError(err_msg)
+
+
+def validate_modules(names: Iterable[str]) -> None:
+    missing_modules = set(filterfalse(module_accessible, names))
+
+    if not missing_modules:
+        return
+
+    missing_modules_str = join_strings(map(wrap_with_quotes,
+                                           missing_modules))
+    err_msg = ('Next modules not found:\n'
+               '{modules}.'
+               .format(modules=missing_modules_str))
+    raise ModuleNotFoundError(err_msg)
+
+
+def module_accessible(name: str,
+                      *,
+                      sep: str = '.') -> bool:
+    sub_modules_names = name.split(sep)
+    package = ''
+    for sub_module_name in sub_modules_names[:-1]:
+        spec = importlib.util.find_spec(sub_module_name, package)
+        if spec is None:
+            return False
+        package += sub_module_name
+
+    return True
