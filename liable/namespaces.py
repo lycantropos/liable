c5a096b2a530038b3731c1fdf0b6800dfd291803
Ibrakov Azat <azatibrakov@gmail.com>
2017-12-19 19:48:21 +0700

    Added 'namespaces' module for searching in mapping-like namespaces

---
+++ liable/namespaces.py
@@ -0,0 +1,47 @@
+import inspect
+from itertools import chain
+from types import ModuleType
+from typing import (Any,
+                    Iterator,
+                    Mapping,
+                    Tuple)
+
+from . import modules
+from .utils import to_name
+
+NamespaceType = Mapping[str, Any]
+
+
+def search(object_: Any,
+           *,
+           namespace: NamespaceType) -> str:
+    if object_ in namespace.values():
+        object_paths = search_objects(object_,
+                                      namespace=namespace)
+    else:
+        object_paths = chain.from_iterable(
+                search_objects(object_,
+                               namespace=vars(module),
+                               module_name=module_name)
+                for module_name, module in namespace_modules(namespace))
+    try:
+        return next(object_paths)
+    except StopIteration as err:
+        raise LookupError('Object "{object}" not found in namespace.'
+                          .format(object=to_name(object_))) from err
+
+
+def search_objects(object_: Any,
+                   *,
+                   namespace: NamespaceType,
+                   module_name: str = '') -> str:
+    for name, content in namespace.items():
+        if content is object_:
+            yield modules.SEPARATOR.join(filter(None, [module_name, name]))
+
+
+def namespace_modules(namespace: NamespaceType
+                      ) -> Iterator[Tuple[str, ModuleType]]:
+    for name, content in namespace.items():
+        if inspect.ismodule(content):
+            yield name, content
