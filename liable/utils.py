300031c22331369dbd111766aeb3687c9f90b65f
Ibrakov Azat <azatibrakov@gmail.com>
2017-12-20 15:02:52 +0700

    Extracted files-specific utilities into 'files' module

--- liable/utils.py
+++ liable/utils.py
@@ -1,34 +1,11 @@
 import os
-from functools import partial
 from typing import (Any,
                     Iterable,
-                    Iterator,
                     Sequence)
 
 from .arboterum import to_tree
 
 STRINGS_SEPARATOR = ',\n'
-
-
-def find_files(path: str,
-               *,
-               recursive: bool) -> Iterator[str]:
-    if os.path.isdir(path):
-        yield from directory_files(path,
-                                   recursive=recursive)
-    else:
-        yield path
-
-
-def directory_files(path: str,
-                    *,
-                    recursive: bool) -> Iterator[str]:
-    if recursive:
-        for root, _, files_names in os.walk(path):
-            yield from map(partial(os.path.join, root), files_names)
-    else:
-        yield from filter(os.path.isfile,
-                          map(partial(os.path.join, path), os.listdir(path)))
 
 
 def join_strings(strings: Iterable[str],
