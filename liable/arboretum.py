05fd00a2075ff04363ff02edee81fb0fa53aa9f9
Ibrakov Azat <azatibrakov@gmail.com>
2017-12-22 00:34:07 +0700

    Added 'import_absolutizer' function factory

--- liable/arboretum.py
+++ liable/arboretum.py
@@ -1,8 +1,10 @@
 import ast
 import operator
+import os
 import pkgutil
 from itertools import chain
 from typing import (Union,
+                    Callable,
                     Iterable,
                     Iterator,
                     Tuple)
@@ -42,6 +44,36 @@ def split_imports(statement: ImportType,
             return package_name
 
         yield from zip(objects_names, map(module_name, objects_names))
+
+
+def import_absolutizer(module_path: str
+                       ) -> Callable[[ast.ImportFrom], ast.ImportFrom]:
+    """Returns operator which makes ``ast.ImportFrom`` statements absolute."""
+    module_directory_path = os.path.dirname(module_path)
+    module_directory_relative_path = catalog.to_relative(module_directory_path)
+
+    def to_absolute(statement: ImportType) -> ImportType:
+        if isinstance(statement, ast.Import):
+            return statement
+        module_name = to_module_name(statement)
+        return ast.ImportFrom(module=module_name,
+                              level=0,
+                              names=statement.names,
+                              lineno=statement.lineno,
+                              col_offset=statement.col_offset)
+
+    def to_module_name(statement: ast.ImportFrom) -> str:
+        module = statement.module
+        if is_import_relative(statement):
+            jumps = (os.pardir + os.sep) * (statement.level - 1)
+            module_path = os.path.join(module_directory_relative_path,
+                                       jumps,
+                                       module or '')
+            module_path = os.path.normpath(module_path)
+            module = catalog.to_import(module_path)
+        return module
+
+    return to_absolute
 
 
 def to_tree(source: str,
