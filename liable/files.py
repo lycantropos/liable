412d9ffae71b2eb36ae6e69491d6aafa38235d24
Ibrakov Azat <azatibrakov@gmail.com>
2018-01-11 18:15:52 +0700

    Added utilities for packages initialization

--- liable/files.py
+++ liable/files.py
@@ -22,3 +22,26 @@ def directory_files(path: str,
     else:
         yield from filter(os.path.isfile,
                           map(partial(os.path.join, path), os.listdir(path)))
+
+
+def make_packages(directory: str,
+                  *sub_directories: str) -> None:
+    test_cases_directory = os.path.join(directory, *sub_directories)
+    os.makedirs(test_cases_directory,
+                exist_ok=True)
+    for package_path in iterative_join(directory, *sub_directories):
+        make_init_module(package_path)
+
+
+def make_init_module(directory: str) -> None:
+    path = os.path.join(directory, '__init__.py')
+    open(path, mode='a').close()
+
+
+def iterative_join(directory: str,
+                   *sub_directories: str) -> Iterator[str]:
+    package_path = directory
+    yield package_path
+    for sub_dir in sub_directories:
+        package_path = os.path.join(package_path, sub_dir)
+        yield package_path
