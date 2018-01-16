9f01beb91bdf46b7df5b3b2107a185f3ee2a0e35
Ibrakov Azat <azatibrakov@gmail.com>
2018-01-17 02:44:30 +0700

    Removed redundant module

--- tests/utils.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-from hypothesis import find
-from hypothesis.searchstrategy import SearchStrategy
-
-
-def example(strategy: SearchStrategy) -> Any:
-    return find(specifier=strategy,
-                condition=lambda x: True)
